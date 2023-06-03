2722e247853ca55596d380bfb9c77b4da0ad101b
ketchadapron <ketchadapron.s@gmail.com>
2023-06-03 13:51:32 -0500

    Add dictionary API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
 import "./Dictionary";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(null);
 
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword} definition`);
+
+    let apiUri = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    console.log(apiUri);
+    axios.get(apiUri).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
