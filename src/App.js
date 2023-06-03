5482b0fb94714c5bfd3fef57adb70592bf160e18
ketchadapron <ketchadapron.s@gmail.com>
2023-06-03 13:34:19 -0500

    Added Search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import "./App.css";
 import logo from "./logo.png";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+
+        <footer className="text-center App-footer">
+          <small>Coded by Ketchadapron</small>
+        </footer>
+      </div>
     </div>
   );
 }
